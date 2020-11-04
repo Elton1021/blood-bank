6c63633956c3a2d26b39a1ee80686348e4d9e9b9
Elton Andrew <elton.andrew@airpay.co.in>
2020-11-04 12:58:13 +0530

    view-request

---
+++ view/view-request.php
@@ -0,0 +1,28 @@
+<?php
+    $viewRequestNav = 'active';
+    require_once('components/headwrapper.php');
+    require_once('../controllers/SampleDetailsController.php');
+    require_once('components/table.php');
+
+    $sample = new SampleDetailsController();
+
+    $data = $sample->getRequests();
+?>
+<div class="container mt-5 mb-5 contentHeight">
+    <div class="card bg-dark text-light shadow">
+        <div class="card-header">
+            <span class="material-icons text-danger" style="vertical-align:text-top;">
+                invert_colors
+            </span>
+            <span style="font-size:18px">
+                Blood Sample Requests
+            </span>
+        </div>
+        <div class="card-body">
+        <?php echo datatable($data[0],$data[1])?>
+        </div>
+    </div>
+</div>
+<?php
+    require_once('components/footwrapper.php');
+?>
